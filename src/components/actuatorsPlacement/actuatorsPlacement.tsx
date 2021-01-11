df0289838359217c45a5f1c49b5ef4b61bd6a0e7
Bruno Fruchard <perso@brunofruchard.com>
2021-01-11 13:26:15 +0100

    feat(3D avatar + camera controls):  added 3D avatar with smooth camera controls to zoom, pan, and rotate the view

--- src/components/actuatorsPlacement/actuatorsPlacement.tsx
+++ src/components/actuatorsPlacement/actuatorsPlacement.tsx
@@ -1,10 +1,9 @@
-import React, { useRef, useState } from 'react';
-import { Canvas, MeshProps, useFrame, useThree, useLoader, extend } from 'react-three-fiber';
+import React, { useRef, Suspense } from 'react';
+import { Canvas, useFrame, useThree, extend } from 'react-three-fiber';
+import {DirectionalLight} from "react-three-fiber/components";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-// @ts-ignore
-import avatar from '/assets/avatar.gltf';
-
+import Model from './avatar';
+import * as THREE from 'three';
 
 extend({OrbitControls});
 
@@ -22,35 +21,65 @@ extend({OrbitControls});
 //     return <OrbitControls ref={controls} args={[camera, domElement]} />;
 // };
 
+//
+// const Box: React.FC<MeshProps> = (props) => {
+//     // This reference will give us direct access to the mesh
+//     const mesh = useRef()
+//
+//     // Set up state for the hovered and active state
+//     const [hovered, setHover] = useState(false)
+//     const [active, setActive] = useState(false)
+//
+//     // Rotate mesh every frame, this is outside of React without overhead
+//     useFrame(() => {
+//         if(mesh.current) { // @ts-ignore
+//             mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
+//         }
+//     })
+//
+//     return (
+//         <mesh
+//             {...props}
+//             ref={mesh}
+//             scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+//             onClick={(event) => setActive(!active)}
+//             onPointerOver={(event) => setHover(true)}
+//             onPointerOut={(event) => setHover(false)}>
+//             <boxBufferGeometry args={[1, 1, 1]} />
+//             <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+//         </mesh>
+//     )
+// }
 
-const Box: React.FC<MeshProps> = (props) => {
-    // This reference will give us direct access to the mesh
-    const mesh = useRef()
 
-    // Set up state for the hovered and active state
-    const [hovered, setHover] = useState(false)
-    const [active, setActive] = useState(false)
-
-    // Rotate mesh every frame, this is outside of React without overhead
-    useFrame(() => {
-        if(mesh.current) { // @ts-ignore
-            mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
-        }
-    })
-
-    return (
-        <mesh
-            {...props}
-            ref={mesh}
-            scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-            onClick={(event) => setActive(!active)}
-            onPointerOver={(event) => setHover(true)}
-            onPointerOut={(event) => setHover(false)}>
-            <boxBufferGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-        </mesh>
-    )
+function rotateLight(camera: THREE.Camera) {
+    const v3 = new THREE.Vector3(4, 4, 4);
+    const lightMatrix = new THREE.Matrix4().makeRotationFromQuaternion(camera.quaternion);
+    return v3.applyMatrix4(lightMatrix);
 }
+
+const CameraControls = () => {
+    // Get a reference to the Three.js Camera, and the canvas html element.
+    // We need these to setup the OrbitControls component.
+    // https://threejs.org/docs/#examples/en/controls/OrbitControls
+    const {
+        camera,
+        gl: { domElement },
+    } = useThree();
+    // Ref to the controls, so that we can update them on every frame using useFrame
+    const controls = useRef();
+    // @ts-ignore
+    useFrame((state) => controls.current.update());
+    // @ts-ignore
+    return  (
+        <group>
+            {/* @ts-ignore */}
+            <orbitControls ref={controls} args={[camera, domElement]} />
+            <DirectionalLight position={rotateLight(camera)} />
+        </group>
+    );
+};
+
 
 export default function ActuatorPlacement() {
     //TODO load the glTF data from the file -- not working but I don't know what's wrong
@@ -60,9 +89,12 @@ export default function ActuatorPlacement() {
     return (
         <Canvas
             camera={{ fov: 35, aspect: 1, near: 0.1, far: 100 , position: [0, 0, 5]}}>
+            <CameraControls/>
             <ambientLight color={0xffffff} intensity={0.5}/>
-            <directionalLight color={0xffffff} intensity={0.5}/>
-            <Box position={[0, 0, 1]}/>
+            <Suspense fallback={null}>
+                <Model/>
+            </Suspense>
+            {/*<Box position={[0, 0, 1]}/>*/}
             {/*<mesh visible geometry={avatar.nodes.Default.geometry}>*/}
             {/*    <meshStandardMaterial*/}
             {/*        attach="material"*/}
