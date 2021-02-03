38a0aa65b98b810ccc3a1c6e1935c69219309a68
Hypnozk <richard_fake@gmx.de>
2021-02-03 14:06:44 +0100

    move tacton information in context

--- src/components/centralComponents/TactonContext.tsx
+++ src/components/centralComponents/TactonContext.tsx
@@ -1,25 +1,86 @@
 import React, { createContext, ReactNode, useContext, useRef, useState, useEffect, MutableRefObject } from 'react';
 import * as THREE from 'three';
+import tactons from '../timeProfile/hardcodedTactons';
+let VTP = require('vtp.js/dist/vtp.cjs');
+
+interface VTPInstruction {
+    type: string,
+    channelSelect: number,
+    timeOffset: number,
+    amplitude: number,
+    frequency: number
+};
+
+interface tactonAttributes {
+    duration: number,
+    actuators: { [key: number]: { [key: string]: number }[] }
+}
+
+
 const TactonContext = createContext<
     {
         actuatorPositions: THREE.Vector3[],
         updateActuators: (actuators: THREE.Vector3[]) => void,
+        rawTacton: ArrayBufferLike,
+        encodedTacton: tactonAttributes,
+        setTacton: () => void
     }>(null);
 
 const TactonProvider = (props: { children: ReactNode }) => {
     const [state, setState] = useState({
         actuatorPositions: [] as THREE.Vector3[],
+        rawTacton: null,
+        encodedTacton: null,
     });
 
     useEffect(() => {
         setState({
+            ...state,
             actuatorPositions: new Array(8).fill(null).map(() => new THREE.Vector3(0, 0, 0))
         })
     }, [])
 
     const updateActuatorsPostion = (actuators: THREE.Vector3[]) => {
         setState({
+            ...state,
             actuatorPositions: actuators
+        })
+    }
+
+    const encodeTacton = (instructionWords: ArrayBufferLike): tactonAttributes => {
+        const actuators: tactonAttributes["actuators"] = {};
+        for (let i = 1; i <= 8; i++) { actuators[i] = []; }
+        let currentTime = 0;
+        //TODO use instruction words
+        // VTP.readInstructionWords(instructionWords)
+        tactons[0].map((el: object, i: number) => VTP.decodeInstruction(el))
+            .map((instruction: VTPInstruction) => {
+                currentTime += instruction.timeOffset;
+                if (instruction.type == 'SetAmplitude') {
+                    if (instruction.channelSelect !== 0) {
+                        actuators[instruction.channelSelect + 1].push({ amplitude: instruction.amplitude, time: currentTime });
+                    }
+                    // channel = 0 means all actuators must change
+                    else {
+                        Object.keys(actuators).forEach((key, i) => actuators[+key].push({ amplitude: instruction.amplitude, time: currentTime }));
+                    }
+                }
+            });
+
+        return { duration: currentTime, actuators };
+    }
+    const setNewTacton = () => {
+        const instructionWords = new Uint8Array([
+            0x10, 0x00, 0x00, 0xEA, 0x20, 0x00, 0x00, 0x7B, 0x10, 0x20, 0x01, 0x59,
+            0x10, 0x20, 0xC9, 0xC8, 0x10, 0x10, 0x03, 0x15, 0x00, 0x00, 0x07, 0xD0,
+            0x20, 0x00, 0x00, 0xEA, 0x10, 0x20, 0x02, 0x37
+        ]).buffer;
+
+        const encodedTacton:tactonAttributes = encodeTacton(instructionWords)
+        setState({
+            ...state,
+            rawTacton: instructionWords,
+            encodedTacton: encodedTacton
         })
     }
 
@@ -27,7 +88,10 @@ const TactonProvider = (props: { children: ReactNode }) => {
         <TactonContext.Provider value={
             {
                 actuatorPositions: state.actuatorPositions,
-                updateActuators: updateActuatorsPostion
+                updateActuators: updateActuatorsPostion,
+                rawTacton: state.rawTacton,
+                encodedTacton: state.encodedTacton,
+                setTacton: setNewTacton,
             }}>
             { props.children}
         </TactonContext.Provider>
--- src/components/timeProfile/timeProfile.tsx
+++ src/components/timeProfile/timeProfile.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import ActuatorTimeProfile from './actuatorTimeProfile';
 import { Grid, Button } from '@material-ui/core';
-let VTP = require('vtp.js/dist/vtp.cjs');
 import { ipcRenderer } from 'electron';
-import tactons from './hardcodedTactons';
+import { TactonContext } from '../centralComponents/TactonContext'
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
@@ -19,53 +18,7 @@ const useStyles = makeStyles((theme: Theme) => ({
   }
 }));
 
-
-interface VTPInstruction {
-    type: string,
-    channelSelect: number,
-    timeOffset: number,
-    amplitude: number,
-    frequency: number
-};
-
-interface tactonAttributes {
-    duration: number,
-    actuators: { [key: number]: { [key: string]: number }[] }
-}
-
-
-function hardcodedData(): tactonAttributes {
-    const instructionWords = new Uint8Array([
-        0x10, 0x00, 0x00, 0xEA, 0x20, 0x00, 0x00, 0x7B, 0x10, 0x20, 0x01, 0x59,
-        0x10, 0x20, 0xC9, 0xC8, 0x10, 0x10, 0x03, 0x15, 0x00, 0x00, 0x07, 0xD0,
-        0x20, 0x00, 0x00, 0xEA, 0x10, 0x20, 0x02, 0x37
-    ]).buffer;
-
-    const actuators: tactonAttributes["actuators"] = {};
-    for(let i = 1; i <= 8; i++) { actuators[i] = []; }
-    let currentTime = 0;
-    //TODO use instruction words
-    // VTP.readInstructionWords(instructionWords)
-    tactons[0].map((el: object, i: number) => VTP.decodeInstruction(el))
-        .map((instruction: VTPInstruction) => {
-            currentTime += instruction.timeOffset;
-            if(instruction.type == 'SetAmplitude') {
-                if(instruction.channelSelect !== 0) {
-                    actuators[instruction.channelSelect+1].push({ amplitude: instruction.amplitude, time: currentTime });
-                }
-                // channel = 0 means all actuators must change
-                else {
-                    Object.keys(actuators).forEach((key, i) => actuators[+key].push({ amplitude: instruction.amplitude, time: currentTime }));
-                }
-            }
-        });
-
-    return { duration: currentTime, actuators };
-}
-
 export default function TimeProfile() {
-  const [ data, setData ] = React.useState<tactonAttributes>(hardcodedData);
-
   const classes = useStyles();
 
   //TODO implement sending tactons from the serial connection
@@ -75,24 +28,31 @@ export default function TimeProfile() {
   });
 
   return (
-      <Grid container item direction="column" alignContent="center" justify="center">
-        <Grid item xs={10} className={classes.fullWidth}>
-          <svg id="time-profile" className={classes.svg}>
-            {(() => {
-              const colors = [ '#ab2056', '#5454ff', '#24ab24', '#9a9a9a', '#9a34ff', '#21abab','#ffff00', '#df8100' ];
-              const startY = 30, stepY = 65;
-              const res = colors.map((col: string, id) => {
-                return <ActuatorTimeProfile key={'timeProfile-'+id} id={id+1}
-                                            x={100} y={startY + stepY*id} width={400} height={60} color={col}
-                                            duration={ data.duration } data={ data.actuators[id+1] }/>
-              });
-              return res;
-            })()}
-          </svg>
+    <TactonContext.Consumer>
+      {({ encodedTacton, setTacton }) => (
+        <Grid container item direction="column" alignContent="center" justify="center">
+          {encodedTacton !== null ?
+            <Grid item xs={10} className={classes.fullWidth}>
+              <svg id="time-profile" className={classes.svg}>
+                {(() => {
+                  const colors = ['#ab2056', '#5454ff', '#24ab24', '#9a9a9a', '#9a34ff', '#21abab', '#ffff00', '#df8100'];
+                  const startY = 30, stepY = 65;
+                  const res = colors.map((col: string, id) => {
+                    return <ActuatorTimeProfile key={'timeProfile-' + id} id={id + 1}
+                      x={100} y={startY + stepY * id} width={400} height={60} color={col}
+                      duration={encodedTacton.duration} data={encodedTacton.actuators[id + 1]} />
+                  });
+                  return res;
+                })()}
+              </svg>
+            </Grid> :
+            <Grid item xs={12}>
+              We don't receive any tactons :(
+          </Grid>}
+          <Button onClick={() => setTacton()}>Change Data</Button>
+          {/*</Grid>*/}
         </Grid>
-        {/*<Grid item xs={1}>*/}
-        {/*  <Button onClick={() => setData(hardcodedData())}>Change Data</Button>*/}
-        {/*</Grid>*/}
-      </Grid>
+      )}
+    </TactonContext.Consumer>
   )
 }
