17cac73fded7b58c02560c23ca908b25d853e4d4
Hypnozk <richard_fake@gmx.de>
2021-03-07 17:08:35 +0100

    add searchbar for download

--- src/components/importLayout/importLayout.tsx
+++ src/components/importLayout/importLayout.tsx
@@ -2,17 +2,17 @@ import React, { useEffect, useContext } from 'react';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import ListItemText from '@material-ui/core/ListItemText';
-import Select from '@material-ui/core/Select';
-import Checkbox from '@material-ui/core/Checkbox';
+import InputAdornment from '@material-ui/core/InputAdornment';
 import List from '@material-ui/core/List'
 import ImportRow from './importRow'
 import axios from 'axios'
 import { TactonContext, Tacton } from '../centralComponents/TactonContext'
+import TextField from '@material-ui/core/TextField';
+import SearchIcon from '@material-ui/icons/Search';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import IconButton from '@material-ui/core/IconButton';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -42,7 +42,10 @@ const useStyles = makeStyles((theme: Theme) =>
         fixedButton: {
             position: 'fixed',
             bottom: 10,
-        }
+        },
+        margin: {
+            margin: theme.spacing(1),
+        },
     }),
 );
 
@@ -54,48 +57,130 @@ interface ImportLayoutProps {
 const ImportLayout = (props: ImportLayoutProps) => {
     const classes = useStyles();
     const { setTacton } = useContext(TactonContext);
-    const [personName, setPersonName] = React.useState<string[]>([]);
     const [tactonList, setTactonList] = React.useState<Tacton[]>([]);
+    const [searchPhrase, setSearchPhrase] = React.useState('');
+    const [tactonDisplay, setTactonDisplay] = React.useState<Tacton[]>([]);
 
     async function getTactonMeta() {
         const res = await axios.get('tactons')
         setTactonList(res.data);
+        setTactonDisplay(res.data)
     }
     useEffect(() => {
         getTactonMeta();
     }, []);
 
-
-    const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-        setPersonName(event.target.value as string[]);
-    };
-
     const importClicked = (tacton: Tacton) => {
-       // console.log('iclicked')
-       // console.log(tacton);
+        // console.log('iclicked')
+        // console.log(tacton);
         setTacton(tacton);
         props.returnMainLayout();
     };
 
-    const tactonRow = (value: Tacton, index: number) => {
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchPhrase(event.target.value);
+    };
+
+    const startSearch = () => {
+        console.log(searchPhrase)
+        const regex = /[a-z|A-Z|0-9|Ä|ä|Ö|ö|Ü|ü]+/g;
+        const found: string[] = searchPhrase.match(regex);
+        console.log(found);
+        let tempTactonList: Tacton[] = [];
+        if (found?.length > 0) {
+            tactonList.forEach((tacton: Tacton) => {
+                for (let i = 0; i < found.length; i++) {
+                    //check if title is found in description or title
+                    if (tacton.title.includes(found[i]) || tacton.description.includes(found[i])) {
+                        tempTactonList.push(tacton);
+                        break;
+                    } else {
+                        let correctTagFound = false;
+                        //check if string is in tags
+                        for (let y = 0; y < tacton.tags.length; y++) {
+                            if (tacton.tags[y].name.includes(found[i])) {
+                                tempTactonList.push(tacton);
+                                correctTagFound = true;
+                                break;
+                            }
+                        }
+                        //string was in tags
+                        if (correctTagFound)
+                            break;
+
+                        //check if string is in bodytags
+                        for (let z = 0; z < tacton.bodytags.length; z++) {
+                            if (tacton.bodytags[z].name.includes(found[i])) {
+                                tempTactonList.push(tacton);
+                                correctTagFound = true;
+                                break;
+                            }
+                        }
+                        //string was in bodytags
+                        if (correctTagFound)
+                            break;
+                    }
+                }
+            })
+            if (tempTactonList.length > 0) {
+                setTactonDisplay(tempTactonList)
+            }
+        } else {
+            if (tactonList.length > 0)
+                setTactonDisplay(tactonList)
+        }
+    }
+
+    const handleKeyPress = (event: any) => {
+        if (event.key === 'Enter') {
+            startSearch()
+            // event.preventDefault();
+        }
+    }
+
+    const tactonRow = (index: number) => {
         if (index % 2 == 0) {
-            if (index + 1 < tactonList.length) {
-                return <ImportRow key={tactonList[index].id} firstTacton={tactonList[index]} secondTacton={tactonList[index + 1]} onClickImport={(tacton) => importClicked(tacton)} />
+            if (index + 1 < tactonDisplay.length) {
+                return <ImportRow key={tactonDisplay[index].id} firstTacton={tactonDisplay[index]} secondTacton={tactonDisplay[index + 1]} onClickImport={(tacton) => importClicked(tacton)} />
             } else {
-                return <ImportRow key={tactonList[index].id} firstTacton={tactonList[index]} secondTacton={null} onClickImport={(tacton) => importClicked(tacton)} />
+                return <ImportRow key={tactonDisplay[index].id} firstTacton={tactonDisplay[index]} secondTacton={null} onClickImport={(tacton) => importClicked(tacton)} />
             }
         } else {
             return null
         }
     }
+
     return (
         // <Grid container spacing={3} hidden={!props.active}>
         <Grid container className={classes.root} style={{ marginBottom: 40 }}>
             <Grid container>
+                <Grid item xs={1} />
+                <Grid item xs={10}>
+                    <FormControl className={classes.margin} fullWidth={true}>
+                        <InputLabel htmlFor="searchfield">Enter your search phrase</InputLabel>
+                        <Input
+                            id="searchfield"
+                            value={searchPhrase}
+                            onChange={handleChange}
+                            onKeyDown={handleKeyPress}
+                            fullWidth={true}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={startSearch}
+                                    >
+                                        <SearchIcon />
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                        />
+                    </FormControl>
+                </Grid>
                 <Grid item xs={12}>
                     <List className={classes.listLeft}>
-                        {tactonList?.map((value, index) => (
-                            tactonRow(value, index)
+                        {tactonDisplay?.map((value, index) => (
+                            tactonRow(index)
                         ))}
                     </List>
                 </Grid>
